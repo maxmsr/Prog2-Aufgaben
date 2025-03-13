038685681d747af2200e2e9c1e1b60fc1ae72112
Max Moser <max.h.moser@gmail.com>
2025-03-13 08:58:00 +0100

    Aufgabe 3 Fertig

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ import Übungsblatt1.*;
 // Frame-Klasse, die die GUI repräsentiert
 public class Main extends JFrame {
     public static void main(String[] args) {
-        //new Aufgabe1();
-        new Aufgabe4();
+        // new Aufgabe1();
+        // new Aufgabe2();
+        new Aufgabe3();
+        // new Aufgabe4();
     }
 }
