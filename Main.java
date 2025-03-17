c0e0429fc36b8b05ed7fb95f145b6153e1915a9e
Max Moser <max.h.moser@gmail.com>
2025-03-17 14:53:38 +0100

    Aufgabe 16 fertig

--- Main.java
+++ Main.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
-
 import Übungsblatt1.*;
 import Übungsblatt5.*;
 
-
-// Frame-Klasse, die die GUI repräsentiert
-public class Main{
+public class Main {
     public static void main(String[] args) {
         // new Aufgabe1();
         // new Aufgabe2();
         // new Aufgabe3();
         // new Aufgabe4();
         // new Aufgabe5();
-        new Aufgabe16();
+
+        Aufgabe16 thread = new Aufgabe16();
+        thread.start();
     }
 }
--- Übungsblatt5/Aufgabe16.java
+++ Übungsblatt5/Aufgabe16.java
@@ -1,8 +1,44 @@
 package Übungsblatt5;
-
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Aufgabe16 extends Thread {
-    Aufgabe16 thread = new Aufgabe16();
 
+    public Aufgabe16() {
+        // Leerer Konstruktor
+    }
 
+    @Override
+    public void run() {
+        // Warte auf Enter-Eingabe bevor der Timer startet
+        System.out.print("To Start the timer, press enter: ");
+        Scanner scan = new Scanner(System.in);
+
+        // Blockiere bis Enter gedrückt wird
+        scan.nextLine();  // Diese Zeile wartet auf Enter
+
+        System.out.println("Timer started!");
+
+        // Timer-Logik
+        int seconds = 0;
+        try {
+            while (true) {
+                // Warten und hochzählen
+                Thread.sleep(1000);
+                seconds++;
+                System.out.println("Timer: " + seconds + " seconds");
+
+                // Prüfen, ob Enter gedrückt wurde
+                if (System.in.available() > 0) {
+                    scan.nextLine(); // Eingabe (Enter) auslesen
+                    System.out.println("Der Timer lief " + Thread.activeCount() + " Sekunden lang.");
+                    break;
+                }
+            }
+        } catch (InterruptedException | IOException e) {
+            System.out.println("Timer interrupted");
+        } finally {
+            scan.close();
+        }
+    }
 }
